3ab3799956d01a3b11b50c28c2fc58315c8528b9
MarvinProInfo <sainterosemarvin@gmail.com>
2023-02-24 12:24:11 +0100

    chore: Removing space

--- src/ajouterArticle.php
+++ src/ajouterArticle.php
@@ -1,8 +1,11 @@
 <?php
+
 require_once ("../Inclusion/header.php");
+
 ?>
 
 <?php
+
 // debug($_POST);
 //On verifie que le formulaire à  été envoyer
 if (!empty($_POST)){
@@ -18,7 +21,7 @@ if (!empty($_POST)){
       }
       //date_format attend deux arguments: une date et un format(dmYHis)
       $imgName = date_format(new DateTime(),'dmYHis').uniqid().$_FILES['image']['name'];
-      copy($_FILES['image']['tmp_name'], "../assets/upload/" . $imgName);
+      copy($_FILES['image']['tmp_name'],"../assets/upload/".$imgName);
 
       //Tableau  des valeur  à envoyer
       $data = [
@@ -27,69 +30,69 @@ if (!empty($_POST)){
         'description' => $_POST['description'],
         'prix' => $_POST['prix'],
         'taille' => $_POST['taille']
-    ];
+      ];
+
       //ici, on crée une variable qui contient notre requête
       $request = "INSERT INTO articles ( nom, image, description, prix, taille) VALUES (:nom, :image, :description, :prix, :taille)";
+      $request2 = "INSERT INTO articles ( nom, image, description, prix, taille) VALUES (:nom, :image, :description, :prix, :taille)";
 
+      $request2 = "INSERT INTO articles ( nom, image, description, prix, taille) VALUES (:nom, :image, :description, :prix, :taille)";
         //on prepare la requete a executer dans la bdd
         $response =$pdo->prepare($request);
+
         //* ici on execute la requête en remplaçant les marqueurs (:nom, :image etc....) par les valeurs de notre tableau data
-
         $response->execute(traitement($data));
-
         $_SESSION['messages']['success'][] = 'Votre article a bien été ajouté';
-
         header('location:../views/article.php');
         exit();
     }
   }
 }
 
-
 ?>
 
-    <div class="container">
-<h1 class="text-center">Ajouter un article</h1>
+<div class="container">
+    <h1 class="text-center">Ajouter un article</h1>
 
-<form method="Post" enctype="multipart/form-data">
-  <fieldset>
+      <form method="Post" enctype="multipart/form-data">
+        <fieldset>
     
-    <div class="form-group">
-      <label for="nom" class="form-label mt-4">Nom de l'article</label>
-      <input type="text" class="form-control" id="nom" placeholder="Nom de l'article" name="nom">
-    </div>
-    <div class="form-group">
-      <label for="prix" class="form-label mt-4">Prix</label>
-      <input type="number" class="form-control" id="prix" placeholder="Prix" min="0" step="0.01" name="prix">
-    </div>
-    <div class="form-group">
-      <label for="description" class="form-label mt-4">Description</label>
-      <textarea class="form-control" id="description" name="description"></textarea>
-    </div>
-    <div class="form-group">
-      <label for="image" class="form-label mt-4">Image</label>
-      <input class="form-control" type="file" id="image" name="image">
-    </div>
-    <div class="form-group">
-      <label for="taille" class="form-label mt-4">Taille</label>
-      <select class="form-select" id="taille" name="taille">
-        <option value="xs">XS</option>
-        <option value="s">S</option>
-        <option value="m">M</option>
-        <option value="l">L</option>
-        <option value="xl">XL</option>
-      </select>
-    </div>
+          <div class="form-group">
+            <label for="nom" class="form-label mt-4">Nom de l'article</label>
+            <input type="text" class="form-control" id="nom" placeholder="Nom de l'article" name="nom">
+          </div>
+          <div class="form-group">
+            <label for="prix" class="form-label mt-4">Prix</label>
+            <input type="number" class="form-control" id="prix" placeholder="Prix" min="0" step="0.01" name="prix">
+          </div>
+          <div class="form-group">
+            <label for="description" class="form-label mt-4">Description</label>
+            <textarea class="form-control" id="description" name="description"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="image" class="form-label mt-4">Image</label>
+            <input class="form-control" type="file" id="image" name="image">
+          </div>
+          <div class="form-group">
+            <label for="taille" class="form-label mt-4">Taille</label>
+            <select class="form-select" id="taille" name="taille">
+            <option value="xs">XS</option>
+            <option value="s">S</option>
+            <option value="m">M</option>
+            <option value="l">L</option>
+            <option value="xl">XL</option>
+            </select>
+          </div>
 
-    <button type="submit" class="btn btn-primary mt-5">Ajouter</button>
-  </fieldset>
-</form>
-
-
-
+          <button type="submit" class="btn btn-primary mt-5">Ajouter</button>
+          
+        </fieldset>
+      </form>
 </div>
 
 
 <?php
+
 require_once ("../Inclusion/footer.php");
+
 ?>
